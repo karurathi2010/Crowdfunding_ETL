6a1bd58e9bac52c96a33c07d03d422f7b38ca936
karurathi2010 <karunakesavan.k@gmail.com>
2024-04-29 13:20:54 -0700

    upload

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -13,9 +13,9 @@ CREATE TABLE "category" (
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
-    "first_name" TEXT   NOT NULL,
-    "last_name" TEXT   NOT NULL,
-    "email" VARCHAR(100)   NOT NULL,
+    "first_name" VARCHAR(30),
+    "last_name" VARCHAR(30),
+    "email" VARCHAR(50),
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
@@ -32,11 +32,11 @@ CREATE TABLE "subcategory" (
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(100)   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
     "description" VARCHAR(250)   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
-    "outcome" VARCHAR(30)   NOT NULL,
+    "outcome" VARCHAR(20)   NOT NULL,
     "backers_count" INT   NOT NULL,
     "country" VARCHAR(30)   NOT NULL,
     "currency" VARCHAR(10)   NOT NULL,
